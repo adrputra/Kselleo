a32e94482934db28b8aec91fd6f0b207c25c3820
adrputra <ricardoandra17@gmail.com>
2022-04-26 08:58:22 +0700

    Login Register

--- API/ViewModel/RegisterVM.cs
+++ API/ViewModel/RegisterVM.cs
@@ -2,5 +2,11 @@
 {
     public class RegisterVM
     {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Image { get; set; }
+        public string Password { get; set; }
     }
 }
