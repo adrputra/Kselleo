0231579b5358c5f8f7cc57b88e3be5bf6affb540
adityaJona <jonaabatman@gmail.com>
2022-04-27 10:38:59 +0700

    admin_user

---
+++ Client/wwwroot/js/Admin/user.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    var table = $('.tableUser').DataTable({
+        "filter": true,
+        "orderMulti": false,
+        "ajax": {
+            "url": "https://localhost:44308/api/users",
+            "datatype": "json",
+            "dataSrc": "result",
+        },
+        "columns": [
+            {
+                "data": "id"
+            },
+            {
+                "data": "image"
+            },
+            {
+                "data": "fullName"
+            },
+            {
+                "data": "email"
+            },
+            {
+                "data": null
+            }
+        ]
+    })
+});
