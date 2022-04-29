e41d154831d1a10c7b898587d517bffe7beedbe5
ramdhanll <erdevlop@gmail.com>
2022-04-29 16:10:31 +0700

    kick and leave member from board

--- API/Controllers/MemberBoardsController.cs
+++ API/Controllers/MemberBoardsController.cs
@@ -1,4 +1,6 @@
-﻿using API.Base;
+﻿using System;
+using System.Net;
+using API.Base;
 using API.Context;
 using API.Models;
 using API.Repository.Data;
@@ -7,13 +9,43 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MemberBoardsController : BaseController<MemberBoard, MemberBoardRepository, int>
-    {
-        public MemberBoardsController(MemberBoardRepository MemberBoardRepository, MyContext myContext) : base(MemberBoardRepository)
-        {
-           
-        }
-    }
+   [Route("api/[controller]")]
+   [ApiController]
+   public class MemberBoardsController : BaseController<MemberBoard, MemberBoardRepository, int>
+   {
+      private MemberBoardRepository memberBoardRepository;
+
+      public MemberBoardsController(MemberBoardRepository MemberBoardRepository, MyContext myContext) : base(MemberBoardRepository)
+      {
+         memberBoardRepository = MemberBoardRepository;
+      }
+
+      [HttpGet("board/{Id}")]
+      public ActionResult GetBoard(int Id)
+      {
+         try
+         {
+            var getMember = memberBoardRepository.GetMemberByBoard(Id);
+            return StatusCode(200, new { code = HttpStatusCode.OK, message = $"Get Member Board By ID Board {Id} Successfully!", data = getMember });
+         }
+         catch (Exception ex)
+         {
+            return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = ex.Message });
+         }
+      }
+
+      [HttpDelete("delete/{id}")]
+      public ActionResult DeleteCustom(int id)
+      {
+         try
+         {
+            var delete = memberBoardRepository.DeleteMember(id);
+            return StatusCode(200, new { code = HttpStatusCode.OK, message = $"Delete Member Board By ID {id} Successfully!" });
+         }
+         catch (Exception ex)
+         {
+            return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = ex.Message });
+         }
+      }
+   }
 }
--- API/Repository/Data/MemberBoardRepository.cs
+++ API/Repository/Data/MemberBoardRepository.cs
@@ -1,16 +1,45 @@
-﻿using API.Context;
+﻿using System;
+using System.Collections;
+using System.Linq;
+using API.Context;
 using API.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace API.Repository.Data
 {
-    public class MemberBoardRepository : GeneralRepository<MyContext, MemberBoard, int>
+   public class MemberBoardRepository : GeneralRepository<MyContext, MemberBoard, int>
 
-    {
-        public MemberBoardRepository(MyContext myContext) : base(myContext)
-        {
-        }
+   {
+      public MemberBoardRepository(MyContext myContext) : base(myContext)
+      {
+         this.myContext = myContext;
+      }
+
+      public IEnumerable GetMemberByBoard(int Id)
+      {
+         var members = myContext.MemberBoards.Include(mb => mb.User).Include(mb => mb.Board).Where(mb => mb.BoardId == Id).ToList();
+         return members;
+      }
+
+      public int DeleteMember(int id)
+      {
+         // get data memberBoards
+         var member = myContext.MemberBoards.SingleOrDefault(mb => mb.Id == id);
+         Console.WriteLine($"MEMBER INVITE => {0}", member);
+         if (member != null)
+         {
+            // delete data verifyInvite by userId and boardId  from memberBoards
+            var verifyInvite = myContext.VerifyInvites.SingleOrDefault(vi => vi.UserId == member.UserId && vi.BoardID == member.BoardId);
+            Console.WriteLine($"VERIFY INVITE => {0}", verifyInvite);
+            myContext.VerifyInvites.Remove(verifyInvite);
+            myContext.MemberBoards.Remove(member);
+            myContext.SaveChanges();
+            return 1;
+         }
+         throw new Exception("Member not found");
+      }
 
 
-    }
+   }
 }
