0e1d32b16c9965f4b3c9d13af7ab4fa8bc4b61f2
adrputra <ricardoandra17@gmail.com>
2022-05-09 11:42:09 +0700

    Add ExpiredToken in Database

---
+++ API/Migrations/20220509044119_add-column-expiredtoken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class addcolumnexpiredtoken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiredToken",
+                table: "Accounts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiredToken",
+                table: "Accounts");
+        }
+    }
+}
