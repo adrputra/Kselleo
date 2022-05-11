b3e4b21030e6febb88508c2a27024ddafdb3267e
adrputra <ricardoandra17@gmail.com>
2022-05-11 13:08:27 +0700

    Error access object

--- API/Controllers/BoardsController.cs
+++ API/Controllers/BoardsController.cs
@@ -2,6 +2,7 @@
 using API.Context;
 using API.Models;
 using API.Repository.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -9,75 +10,93 @@ using System.Net;
 
 namespace API.Controllers
 {
-   [Route("api/[controller]")]
-   [ApiController]
-   public class BoardsController : BaseController<Board, BoardRepository, string>
-   {
-      private BoardRepository boardRepository;
-      public BoardsController(BoardRepository boardRepository, MyContext myContext) : base(boardRepository)
-      {
-         this.boardRepository = boardRepository;
-      }
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardsController : BaseController<Board, BoardRepository, string>
+    {
+        private BoardRepository boardRepository;
+        public BoardsController(BoardRepository boardRepository, MyContext myContext) : base(boardRepository)
+        {
+            this.boardRepository = boardRepository;
+        }
 
-      [HttpGet("user/{Id}")]
-      public ActionResult GetBoard(int Id)
-      {
-         try
-         {
-            // var board = boardRepository.GetBoardByMember(Id);
-            var board = boardRepository.GetBoardByMember2(Id);
-            return StatusCode(200, new { code = HttpStatusCode.OK, message = $"Get Board By User {Id} Successfully!", data = board });
-         }
-         catch (Exception ex)
-         {
-            return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = ex.Message });
-         }
-         //return Ok(_accountRepository.GetMaster(NIK));
-      }
+        [HttpGet("user/{Id}")]
+        public ActionResult GetBoard(int Id)
+        {
+            try
+            {
+                // var board = boardRepository.GetBoardByMember(Id);
+                var board = boardRepository.GetBoardByMember2(Id);
+                return StatusCode(200, new { code = HttpStatusCode.OK, message = $"Get Board By User {Id} Successfully!", data = board });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = ex.Message });
+            }
+            //return Ok(_accountRepository.GetMaster(NIK));
+        }
 
-      [HttpGet("creator/{Id}")]
-      public ActionResult GetBoardCreatedBy(int Id)
-      {
-         try
-         {
-            var board = boardRepository.GetBoardByCreator(Id);
-            return StatusCode(200, new { code = HttpStatusCode.OK, message = $"Get Board By User {Id} Successfully!", data = board });
-         }
-         catch (Exception ex)
-         {
-            return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = ex.Message });
-         }
-         //return Ok(_accountRepository.GetMaster(NIK));
-      }
+        [HttpGet("creator/{Id}")]
+        public ActionResult GetBoardCreatedBy(int Id)
+        {
+            try
+            {
+                var board = boardRepository.GetBoardByCreator(Id);
+                return StatusCode(200, new { code = HttpStatusCode.OK, message = $"Get Board By User {Id} Successfully!", data = board });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = ex.Message });
+            }
+            //return Ok(_accountRepository.GetMaster(NIK));
+        }
 
-      [HttpPost("create")]
-      public ActionResult CreateBoard(Board board)
-      {
-         try
-         {
-            var result = boardRepository.CreateBoard(board);
-            return StatusCode(200, new { code = HttpStatusCode.OK, message = "Board created successfully", data = result });
-         }
-         catch (Exception ex)
-         {
-            return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = ex.Message });
-         }
-         //return Ok(boardRepository.CreateBoard(board));
-      }
+        [HttpPost("create")]
+        public ActionResult CreateBoard(Board board)
+        {
+            try
+            {
+                var result = boardRepository.CreateBoard(board);
+                return StatusCode(200, new { code = HttpStatusCode.OK, message = "Board created successfully", data = result });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = ex.Message });
+            }
+            //return Ok(boardRepository.CreateBoard(board));
+        }
 
-      [HttpGet("detail/{Id}")]
-      public ActionResult GetBoardDetailById(string Id)
-      {
-         try
-         {
-            var board = boardRepository.GetBoardDetailById(Id);
-            return StatusCode(200, new { code = HttpStatusCode.OK, message = $"Get Board By Id {Id} Successfully!", data = board });
-         }
-         catch (Exception ex)
-         {
-            return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = ex.Message });
-         }
-      }
+        //[HttpGet("detail/{Id}")]
+        //public ActionResult GetBoardDetailById(string Id)
+        //{
+        //    try
+        //    {
+        //        var token = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+        //        var auth = boardRepository.AuthenticateMember(token,Id);
+        //        if (auth)
+        //        {
+        //            var board = boardRepository.GetBoardDetailById(Id);
+        //            return StatusCode(200, new { code = HttpStatusCode.OK, message = $"Get Board By Id {Id} Successfully!", data = board });
+        //        }
+        //        else
+        //        {
+        //            return Unauthorized();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(500, new { status = HttpStatusCode.InternalServerError, message = ex.Message });
+        //    }
+        //}
 
-   }
+        [HttpGet("Token/{Id}")]
+        public ActionResult GetToken(string Id)
+        {
+            var token = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            var auth = boardRepository.AuthenticateMember(token, Id);
+            return StatusCode(200, new { code = HttpStatusCode.OK, message = $"Get Token Successfully!", data = auth });
+        }
+
+    }
 }
