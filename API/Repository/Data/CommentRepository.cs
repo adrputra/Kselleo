67cb4b393bcd16937e54c5fde0c7430ed53d5a37
adrputra <ricardoandra17@gmail.com>
2022-05-12 15:10:39 +0700

    Add Comments

--- API/Repository/Data/CommentRepository.cs
+++ API/Repository/Data/CommentRepository.cs
@@ -1,6 +1,8 @@
 ﻿using API.Context;
 using API.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
 
 namespace API.Repository.Data
 {
@@ -9,8 +11,20 @@ namespace API.Repository.Data
    {
       public CommentRepository(MyContext myContext) : base(myContext)
       {
+            this.myContext = myContext;
       }
 
+        public object SendComment(Comment comment)
+        {
+            myContext.Comments.Add(comment);
+            myContext.SaveChanges();
 
-   }
+            return myContext.Comments
+                .Include(u => u.User)
+                .AsSplitQuery()
+                .OrderByDescending(x => x.CreatedAt)
+                .FirstOrDefault(x => x.Id == comment.Id);
+        }
+
+    }
 }
